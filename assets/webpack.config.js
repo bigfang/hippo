5d15397db7704c5cb97e05a8f03fe0c9d6044abc
bigfang <bitair@gmail.com>
2017-09-14 22:43:47 +0800

    添加client

---
+++ assets/webpack.config.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const vuetifyPath = path.resolve(__dirname, './node_modules/vuetify')
+
+module.exports = {
+  devtool: 'source-map',
+  stats: { children: false },
+  entry: {
+    app: ['css/app.css', 'js/app.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, '../priv/static'),
+    filename: 'js/[name].js'
+  },
+  resolve: {
+    modules: ['node_modules', __dirname],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      'phoenix': path.resolve(__dirname, '../deps/phoenix/priv/static/phoenix.js'),
+      'phoenix_html': path.resolve(__dirname, '../deps/phoenix_html/priv/static/phoenix_html.js')
+    },
+    extensions: ['.js', '.vue']
+  },
+  module: {
+    rules: [{
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      }, {
+        test: /\.js$/,
+        include: [vuetifyPath],
+        exclude: [/node_modules/, path.resolve(__dirname, '../deps/')],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }
+      }, {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          extractCSS: true,
+          loaders: {
+          },
+          include: [vuetifyPath]
+        }
+      }, {
+        test: /\.(png|jpg|gif)$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8192
+          }
+        }]
+      },
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('css/[name].css'),
+    new CopyWebpackPlugin([{
+      from: 'static'
+    }]),
+    new webpack.optimize.ModuleConcatenationPlugin()
+  ]
+}
+
+if (process.env.MIX_ENV === 'prod') {
+  module.exports.devtool = '#source-map'
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new UglifyJSPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    })
+  ])
+}
--- config/dev.exs
+++ config/dev.exs
@@ -11,7 +11,8 @@ config :hippo, HippoWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: []
+  watchers: [node: ["node_modules/webpack/bin/webpack.js", "--stdin", "--color",
+                    cd: Path.expand("../assets", __DIR__)]]
 
 # ## SSL Support
 #
